e1d88df50686453215de2fd6970e86233784de29
willeke.acampo <willeke.acampo@nina.no>
2023-03-30 17:23:39 +0200

    chore(src): clean files and move to test add TODOs

--- src/main.py
+++ src/main.py
@@ -7,4 +7,13 @@ from data import prepare_lidar
 
 ## TODO add the other 4 files as subprocesses to make_data
 kommune = input("Enter the municipality: ")
+
+## PREPARE LIDAR DATA
 prepare_lidar.main(kommune)
+
+## PREPARE KOMMUNE DATA
+
+## RUN SEGMENTATION
+
+
+## JOIN
