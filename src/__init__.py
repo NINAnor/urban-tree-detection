74316a72d29d6af668ec9b2227d827548153001f
willeke.acampo <willeke.acampo@nina.no>
2023-05-10 16:33:47 +0200

    add(src/utils): arcpy_utils
    added helper functions for the arcpy lib.
    - df_to_lookupDict
    - reclassify_row
    - join_and_copy

--- src/__init__.py
+++ src/__init__.py
@@ -9,11 +9,12 @@ __version__ = '0.1.0'
 # import specific functions and classes to  
 # they can be imported from other parts of the projects using the syntax "from src import ..." 
 from src.utils.checkExist import createGDB_ifNotExists, fieldExist, addField_ifNotExists,calculateField_ifEmpty,check_isNull
-from src.utils.joinLayer import join_and_copy
+#from src.utils.joinLayer import join_and_copy
 from src.utils.logger import setup_logger 
 from src.utils.config import FKB_BUILDING_PATH, FKB_WATER_PATH, SSB_DISTRICT_PATH, AR5_LANDUSE_PATH
 from src.utils.config import DATA_PATH, RAW_PATH, INTERIM_PATH, PROCESSED_PATH
 from src.utils.config import LOG_PATH
-
+from src.utils.config import MUNICIPALITY, ADMIN_GDB_PATH
+from src.utils.arcpy_utils import reclassify_row, join_and_copy, df_to_lookupDict
 from src import prepare_lidar
 #from .models import watershed_tree
--- src/utils/arcpy_utils.py
+++ src/utils/arcpy_utils.py
@@ -1,7 +1,39 @@
+"""util functions for working with arcpy."""
 import arcpy
 
-         
+def df_to_lookupDict(df, key, value):
+    lookup_dict = {}
+    for index, row in df.iterrows():
+        lookup_dict[row[key]] = row[value]
+    return lookup_dict
 
+def reclassify_row(fc, field_to_check, field_to_modify, lookup_dict):
+    """Reclassifies values in a specified field based on a lookup table.
+
+    Args:
+        fc (str): The path to the feature class.
+        field_to_check (str): The name of the field to check against the lookup table.
+        field_to_modify (str): The name of the field to modify based on the lookup table.
+        lookup_dict (dict): A dictionary object containing lookup values as keys and their corresponding new values as values.
+    """
+    # Create a case-insensitive dictionary for the lookup values
+    lookup_dict_case_insensitive = {k.lower(): v for k, v in lookup_dict.items()}
+
+    # Create an update cursor to modify the values in the field
+    fields = [field_to_check, field_to_modify]
+    #sql_clause = f"{field_to_check} IS NOT NULL"
+    with arcpy.da.UpdateCursor(fc, fields) as cursor:
+        for row in cursor:
+            lookup_value = lookup_dict_case_insensitive.get(str(row[0]).lower())
+            if lookup_value is not None:
+                row[1] = lookup_value
+                cursor.updateRow(row)
+                #print(f"Reclassified value <{row[0]}> to value <{lookup_value}>") 
+
+    # Print a message indicating the update is complete
+    print(f"The rows in <{field_to_modify}> are reclassified.")
+    
+    
 # Join "source_table" to "destination table" and copy attributes from "source table" to attributes in "destination table"
 def join_and_copy(t_dest:str, join_a_dest:str, t_src:str, join_a_src:str, a_src:list, a_dest:list):
     """_summary_
@@ -34,11 +66,13 @@ def join_and_copy(t_dest:str, join_a_dest:str, t_src:str, join_a_src:str, a_src:
             l_dest, 
             name_dest + "." + dest_field, 
             "!" + name_src + "." + src_field + "!"
-        )  
+        )
 
-# =========================== #
-# Join table 2 to table 1 and copy source field from table 2 to destination field of table 1
-# =========================== #
+# --------------------------------------------------------------------------- #
+# Join table 2 to table 1 and copy src field from table 2 to dest. field in table 1
+# TODO check this function and remove 
+# --------------------------------------------------------------------------- #
+
 def join_and_copy2(target_table, key_target, join_table, key_join, field_list_join, field_list_target):
      
     name1 = arcpy.Describe(target_table).name
