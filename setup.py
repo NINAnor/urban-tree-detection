8ec1752920fdf0d4b823c29c24fc1cb45293ecb1
willeke.acampo <willeke.acampo@nina.no>
2023-05-10 13:39:18 +0200

    conda env: update settings yml

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = [
 ]
 
 setup(
-    name='treeDetection',
+    name='src',
     version='0.1.0',
     description="This project provides a workflow for preparing an input dataset for i-Tree Eco analysis using a municipal tree dataset supplemented with lidar-segmented tree crown geometry and auxiliary GIS datasets. ",
     license="MIT",
