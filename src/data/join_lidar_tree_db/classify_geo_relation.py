0304ca4ca8cb497fbb8f32c6cb3a1d289cdc12d2
willeke.acampo <willeke.acampo@nina.no>
2023-05-11 10:48:57 +0200

    update(src/data): classify relation

--- src/data/join_lidar_tree_db/classify_geo_relation.py
+++ src/data/join_lidar_tree_db/classify_geo_relation.py
@@ -40,13 +40,14 @@ logger.setup_logger(logfile=False)
 
 # set path variables
 ds_urban_trees = os.path.join(URBAN_TREES_GDB, "urban_trees")
+ds_joined_trees = os.path.join(URBAN_TREES_GDB, "joined_trees")
 
 fc_urban_trees = os.path.join(URBAN_TREES_GDB, "urban_trees", "urban_trees") # joined dataset
 fc_case_1_2 = os.path.join(ds_urban_trees, "join_case_1_2") 
-fc_case_1 = os.path.join(ds_urban_trees, "join_case_1") 
-fc_case_2 = os.path.join(ds_urban_trees, "join_case_2") 
-fc_case_3 = os.path.join(ds_urban_trees, "join_case_3")
-fc_case_4 = os.path.join(ds_urban_trees, "join_case_4")
+fc_case_1 = os.path.join(ds_joined_trees, "join_case_1") 
+fc_case_2 = os.path.join(ds_joined_trees, "join_case_2") 
+fc_case_3 = os.path.join(ds_joined_trees, "join_case_3")
+fc_case_4 = os.path.join(ds_joined_trees, "join_case_4")
 
 # env settings
 env.overwriteOutput = True
@@ -58,6 +59,61 @@ au.addField_ifNotExists(
     fieldname="geo_relation",
     type="TEXT"
     )
+
+# Unique count of JOIN FID and TARGET FID
+# add field
+au.addField_ifNotExists(
+    featureclass= fc_urban_trees,
+    fieldname= "COUNT_JOIN_FID",
+    type= "LONG"
+)
+au.addField_ifNotExists(
+    featureclass= fc_urban_trees,
+    fieldname= "COUNT_TARGET_FID",
+    type= "LONG"
+    
+)
+
+# --------------------------------------------------------------------------- #
+# Calculate TARGET and JOIN COUNT
+# --------------------------------------------------------------------------- #
+
+if au.fieldExist(fc_urban_trees, "COUNT_JOIN_FID") or au.fieldExist(fc_urban_trees, "COUNT_TRAGET_FID"):
+    arcpy.AddMessage(f"The fields COUNT_JOIN_FID and COUNT_TARGET_FID exists in {os.path.basename(fc_urban_trees)}. Continue... ")
+else:
+    # calculate count JOIN_FID 
+    join_fid_count_stats = arcpy.Statistics_analysis(
+        in_table= fc_urban_trees, 
+        out_table="in_memory/join_fid_count_table",
+        statistics_fields= [["JOIN_FID", "COUNT"]], 
+        case_field="TARGET_FID"
+        )
+
+    # add COUNT_JOIN_FID as field to fc
+    arcpy.JoinField_management(
+        in_data=fc_urban_trees, 
+        in_field="TARGET_FID", 
+        join_table=join_fid_count_stats,
+        join_field="TARGET_FID", 
+        fields="COUNT_JOIN_FID"
+        )
+
+    # calculate count TARGET_FID 
+    target_fid_count_stats = arcpy.Statistics_analysis(
+        fc_urban_trees, 
+        "in_memory/target_fid_count_table", 
+        [["TARGET_FID", "COUNT"]], 
+        "JOIN_FID"
+        )
+    # add COUNT_TARGET_FID as field to fc 
+    arcpy.JoinField_management(
+        fc_urban_trees, 
+        "JOIN_FID", 
+        target_fid_count_stats, 
+        "JOIN_FID", 
+        "COUNT_TARGET_FID"
+        )
+
 
 #------------------------------------------------------ #
 # Classify the geometrical relatisonship
@@ -72,30 +128,45 @@ field_crown_id = "crown_id_laser"
 field_tree_id = "tree_id"
 field_geo_relation = "geo_relation"
 
-# Select case 4 and export to new fc 
+# Select and export case 4 (1:0) 
 if arcpy.Exists(fc_case_4):
     arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_4)} already exists. Continue...")
 else:
-    arcpy.SelectLayerByAttribute_management("lyr", "NEW_SELECTION", "{} IS NOT NULL AND {} IS NULL".format(field_crown_id, field_tree_id))
+    query = f"{field_crown_id} IS NOT NULL AND {field_tree_id} IS NULL"
+    arcpy.management.SelectLayerByAttribute(
+        in_layer_or_view="lyr",
+        selection_type="NEW_SELECTION",
+        where_clause=query,
+        invert_where_clause=None
+    )
     arcpy.CopyFeatures_management("lyr", fc_case_4)
+    au.calculateField_ifEmpty(fc_case_4, field=field_geo_relation, expression= "Case 4")
     logging.info(f'Case 4 selected and exported to {os.path.basename(fc_case_4)}')
+
 
 # Select case 3 and export to new fc 
 if arcpy.Exists(fc_case_3):
     arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_3)} already exists. Continue...")
 else:
-    arcpy.SelectLayerByAttribute_management("lyr", "NEW_SELECTION", "{} IS NULL AND {} IS NOT NULL".format(field_crown_id, field_tree_id))
+    query = f"{field_crown_id} IS NULL AND {field_tree_id} IS NOT NULL"
+    arcpy.management.SelectLayerByAttribute(
+        in_layer_or_view="lyr",
+        selection_type="NEW_SELECTION",
+        where_clause=query,
+        invert_where_clause=None
+    )
     arcpy.CopyFeatures_management("lyr", fc_case_3)
+    au.calculateField_ifEmpty(fc_case_4, field=field_geo_relation, expression= "Case 3")
     logging.info(f'Case 3 selected and exported to {os.path.basename(fc_case_3)}')
 
 # Select case 1 and case 2 and export to new fc 
-if arcpy.Exists(fc_case_1_2):
-    arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_1_2)} already exists. Continue...")
-else:
-    arcpy.SelectLayerByAttribute_management("lyr", "NEW_SELECTION", "{} IS NOT NULL AND {} IS NOT NULL".format(field_crown_id, field_tree_id))
-    arcpy.SelectLayerByAttribute_management("lyr", "ADD_TO_SELECTION", "{} IS NULL AND {} IS NOT NULL".format(field_crown_id, field_tree_id))
-    arcpy.CopyFeatures_management("lyr", fc_case_1_2)
-    logging.info(f'Case 1 and 2 selected and exported to {os.path.basename(fc_case_1_2)}')
+#if arcpy.Exists(fc_case_1_2):
+#    arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_1_2)} already exists. Continue...")
+#else:
+#    arcpy.SelectLayerByAttribute_management("lyr", "NEW_SELECTION", "{} IS NOT NULL AND {} IS NOT NULL".format(field_crown_id, field_tree_id))
+#    arcpy.SelectLayerByAttribute_management("lyr", "ADD_TO_SELECTION", "{} IS NULL AND {} IS NOT NULL".format(field_crown_id, field_tree_id))
+#    arcpy.CopyFeatures_management("lyr", fc_case_1_2)
+#    logging.info(f'Case 1 and 2 selected and exported to {os.path.basename(fc_case_1_2)}')
 
     
 # Separate case 1 and case 2 and export to new fcs 
@@ -153,7 +224,6 @@ else:
         "COUNT_TARGET_FID"
         )
 
-
 # Select features in class 1
 if arcpy.Exists(fc_case_1):
     arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_1)} already exists. Continue...")
@@ -167,9 +237,6 @@ else:
     )
     arcpy.CopyFeatures_management("lyr2", fc_case_1)
     logging.info(f'Case 1 selected and exported to {os.path.basename(fc_case_1)}')
-
-
-
 
 # Select features in class 2
 if arcpy.Exists(fc_case_2):
--- src/data/make_gdb.py
+++ src/data/make_gdb.py
@@ -10,13 +10,23 @@ geodatabase_locations = [ADMIN_GDB, IN_SITU_TREES_GDB, LASER_TREES_GDB, URBAN_TR
 for gdb in geodatabase_locations:
     au.createGDB_ifNotExists(gdb)
 
-print(f"All geodatabases exist for {MUNICIPALITY}")
+print(f"All geodatabases exist for {MUNICIPALITY} municipality.")
 
 ds_urban_trees = os.path.join(URBAN_TREES_GDB, "urban_trees")
+
 
 # Create the "urban_trees" feature dataset if it doesn't exist
 if not arcpy.Exists(ds_urban_trees):
     arcpy.CreateFeatureDataset_management(
         out_dataset_path=URBAN_TREES_GDB,
         out_name= "urban_trees",
-        SPATIAL_REFERENCE=SPATIAL_REFERENCE)
+        spatial_reference=SPATIAL_REFERENCE)
+    
+
+# Create the "joined_trees" feature dataset if it doesn't exist
+ds_joined_trees = os.path.join(URBAN_TREES_GDB, "joined_trees")
+if not arcpy.Exists(ds_joined_trees):
+    arcpy.CreateFeatureDataset_management(
+        out_dataset_path=URBAN_TREES_GDB,
+        out_name= "joined_trees",
+        spatial_reference=SPATIAL_REFERENCE)
