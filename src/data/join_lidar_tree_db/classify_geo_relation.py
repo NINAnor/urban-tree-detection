bb054d20a98ae3fdeef1e93b68e65176e69e693e
willeke.acampo <willeke.acampo@nina.no>
2023-05-11 12:25:05 +0200

    update(src/data): join case 1,2, 3, 4

--- src/data/join_lidar_tree_db/classify_geo_relation.py
+++ src/data/join_lidar_tree_db/classify_geo_relation.py
@@ -1,9 +1,10 @@
 """ 
 Classify the geometric relation between the stem points (in situ) and the crown polygons (laser)
-Case 1: one polygon contains one point (1:1), simple join.
+Case 1: one polygon contains one point (1:1), simple join.  
 Case 2: one polygon contains more than one point (1:n), split crown with voronoi tesselation.
 Case 3: a point is not overlapped by any polygon (0:1), model tree crown using oslo formula.
 Case 4: a polygon does not contain any point (1:0), not used to train i-tree eco/dataset for extrapolation.
+
 result: geo_relation (attr. field in urban_trees feature class) 
 
 Bodo:
@@ -78,41 +79,38 @@ au.addField_ifNotExists(
 # Calculate TARGET and JOIN COUNT
 # --------------------------------------------------------------------------- #
 
-if au.fieldExist(fc_urban_trees, "COUNT_JOIN_FID") or au.fieldExist(fc_urban_trees, "COUNT_TRAGET_FID"):
-    arcpy.AddMessage(f"The fields COUNT_JOIN_FID and COUNT_TARGET_FID exists in {os.path.basename(fc_urban_trees)}. Continue... ")
-else:
-    # calculate count JOIN_FID 
-    join_fid_count_stats = arcpy.Statistics_analysis(
-        in_table= fc_urban_trees, 
-        out_table="in_memory/join_fid_count_table",
-        statistics_fields= [["JOIN_FID", "COUNT"]], 
-        case_field="TARGET_FID"
-        )
+# calculate COUNT_JOIN_FID
+join_fid_count_stats = arcpy.Statistics_analysis(
+    in_table= fc_urban_trees, 
+    out_table="in_memory/join_fid_count_table",
+    statistics_fields= [["JOIN_FID", "COUNT"]], 
+    case_field="TARGET_FID"
+    )
 
-    # add COUNT_JOIN_FID as field to fc
-    arcpy.JoinField_management(
-        in_data=fc_urban_trees, 
-        in_field="TARGET_FID", 
-        join_table=join_fid_count_stats,
-        join_field="TARGET_FID", 
-        fields="COUNT_JOIN_FID"
-        )
+# add COUNT_JOIN_FID as field to fc
+arcpy.JoinField_management(
+    in_data=fc_urban_trees, 
+    in_field="TARGET_FID", 
+    join_table=join_fid_count_stats,
+    join_field="TARGET_FID", 
+    fields="COUNT_JOIN_FID"
+    )
 
-    # calculate count TARGET_FID 
-    target_fid_count_stats = arcpy.Statistics_analysis(
-        fc_urban_trees, 
-        "in_memory/target_fid_count_table", 
-        [["TARGET_FID", "COUNT"]], 
-        "JOIN_FID"
-        )
-    # add COUNT_TARGET_FID as field to fc 
-    arcpy.JoinField_management(
-        fc_urban_trees, 
-        "JOIN_FID", 
-        target_fid_count_stats, 
-        "JOIN_FID", 
-        "COUNT_TARGET_FID"
-        )
+# calculate COUNT_TARGET_FID
+target_fid_count_stats = arcpy.Statistics_analysis(
+    fc_urban_trees, 
+    "in_memory/target_fid_count_table", 
+    [["TARGET_FID", "COUNT"]], 
+    "JOIN_FID"
+    )
+# add COUNT_TARGET_FID as field to fc 
+arcpy.JoinField_management(
+    fc_urban_trees, 
+    "JOIN_FID", 
+    target_fid_count_stats, 
+    "JOIN_FID", 
+    "COUNT_TARGET_FID"
+    )
 
 
 #------------------------------------------------------ #
@@ -121,6 +119,8 @@ else:
 # TODO split case 1 and case 2
 # ----------------------------------------------------- #
 
+
+
 # Create a feature layer from the input feature class
 arcpy.MakeFeatureLayer_management(fc_urban_trees, "lyr")
 
@@ -128,15 +128,78 @@ field_crown_id = "crown_id_laser"
 field_tree_id = "tree_id"
 field_geo_relation = "geo_relation"
 
+query_case_1 = "COUNT_TARGET_FID = 1 And COUNT_JOIN_FID = 1"            # (polygon:point = 1:1)
+query_case_2 = "COUNT_TARGET_FID = 1 And COUNT_JOIN_FID > 1"            # (polygon:point = 1:n)
+query_case_3 = f"{field_crown_id} IS NULL AND {field_tree_id} IS NOT NULL"  # (polygon:point = 0:1) 
+query_case_4 = f"{field_crown_id} IS NOT NULL AND {field_tree_id} IS NULL"  # (polygon:point = 1:0)
+
+# --------------------------------------------------------------------------- #
+# CASE 1 
+# --------------------------------------------------------------------------- #
+
+# Select features in class 1
+if arcpy.Exists(fc_case_1):
+    arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_1)} already exists. Continue...")
+else:   
+    arcpy.management.SelectLayerByAttribute(
+        in_layer_or_view="lyr",
+        selection_type="NEW_SELECTION",
+        where_clause=query_case_1,
+        invert_where_clause=None
+    )
+    arcpy.CopyFeatures_management("lyr", fc_case_1)
+    au.calculateField_ifEmpty(fc_case_1, field=field_geo_relation, expression= "Case 1")
+    logging.info(f'Case 1 selected and exported to {os.path.basename(fc_case_1)}')
+
+# --------------------------------------------------------------------------- #
+# CASE 2 
+# --------------------------------------------------------------------------- #
+
+# Select features in class 2
+if arcpy.Exists(fc_case_2):
+    arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_2)} already exists. Continue...")
+else:   
+    # Select features in class 1
+
+    arcpy.management.SelectLayerByAttribute(
+        in_layer_or_view="lyr",
+        selection_type="NEW_SELECTION",
+        where_clause=query_case_2,
+        invert_where_clause=None
+    )
+    arcpy.CopyFeatures_management("lyr", fc_case_2)
+    au.calculateField_ifEmpty(fc_case_2, field=field_geo_relation, expression= "Case 2")
+    logging.info(f'Case 2 selected and exported to {os.path.basename(fc_case_2)}') 
+
+# --------------------------------------------------------------------------- #
+# CASE 3 
+# --------------------------------------------------------------------------- #
+
+# Select and export case 3 (0:1) 
+if arcpy.Exists(fc_case_3):
+    arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_3)} already exists. Continue...")
+else:
+    arcpy.management.SelectLayerByAttribute(
+        in_layer_or_view="lyr",
+        selection_type="NEW_SELECTION",
+        where_clause=query_case_3,
+        invert_where_clause=None
+    )
+    arcpy.CopyFeatures_management("lyr", fc_case_3)
+    au.calculateField_ifEmpty(fc_case_3, field=field_geo_relation, expression= "Case 3")
+    logging.info(f'Case 3 selected and exported to {os.path.basename(fc_case_3)}')
+
+# --------------------------------------------------------------------------- #
+# CASE 4 
+# --------------------------------------------------------------------------- #
 # Select and export case 4 (1:0) 
 if arcpy.Exists(fc_case_4):
     arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_4)} already exists. Continue...")
 else:
-    query = f"{field_crown_id} IS NOT NULL AND {field_tree_id} IS NULL"
     arcpy.management.SelectLayerByAttribute(
         in_layer_or_view="lyr",
         selection_type="NEW_SELECTION",
-        where_clause=query,
+        where_clause=query_case_4,
         invert_where_clause=None
     )
     arcpy.CopyFeatures_management("lyr", fc_case_4)
@@ -144,20 +207,6 @@ else:
     logging.info(f'Case 4 selected and exported to {os.path.basename(fc_case_4)}')
 
 
-# Select case 3 and export to new fc 
-if arcpy.Exists(fc_case_3):
-    arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_3)} already exists. Continue...")
-else:
-    query = f"{field_crown_id} IS NULL AND {field_tree_id} IS NOT NULL"
-    arcpy.management.SelectLayerByAttribute(
-        in_layer_or_view="lyr",
-        selection_type="NEW_SELECTION",
-        where_clause=query,
-        invert_where_clause=None
-    )
-    arcpy.CopyFeatures_management("lyr", fc_case_3)
-    au.calculateField_ifEmpty(fc_case_4, field=field_geo_relation, expression= "Case 3")
-    logging.info(f'Case 3 selected and exported to {os.path.basename(fc_case_3)}')
 
 # Select case 1 and case 2 and export to new fc 
 #if arcpy.Exists(fc_case_1_2):
@@ -169,89 +218,89 @@ else:
 #    logging.info(f'Case 1 and 2 selected and exported to {os.path.basename(fc_case_1_2)}')
 
     
-# Separate case 1 and case 2 and export to new fcs 
-# Make a feature layer
-arcpy.MakeFeatureLayer_management(fc_case_1_2, "lyr2")
+# # Separate case 1 and case 2 and export to new fcs 
+# # Make a feature layer
+# arcpy.MakeFeatureLayer_management(fc_case_1_2, "lyr2")
 
 
-# Unique count of JOIN FID and TARGET FID
-# add field
-au.addField_ifNotExists(
-    featureclass= fc_case_1_2,
-    fieldname= "COUNT_JOIN_FID",
-    type= "LONG"
-)
-au.addField_ifNotExists(
-    featureclass= fc_case_1_2,
-    fieldname= "COUNT_TARGET_FID",
-    type= "LONG"
+# # Unique count of JOIN FID and TARGET FID
+# # add field
+# au.addField_ifNotExists(
+#     featureclass= fc_case_1_2,
+#     fieldname= "COUNT_JOIN_FID",
+#     type= "LONG"
+# )
+# au.addField_ifNotExists(
+#     featureclass= fc_case_1_2,
+#     fieldname= "COUNT_TARGET_FID",
+#     type= "LONG"
     
-)
+# )
 
-if au.fieldExist(fc_case_1_2, "COUNT_JOIN_FID") or au.fieldExist(fc_case_1_2, "COUNT_TRAGET_FID"):
-    arcpy.AddMessage(f"The fields COUNT_JOIN_FID and COUNT_TARGET_FID exists in {os.path.basename(fc_case_1_2)}. Continue... ")
-else:
-    # calculate count JOIN_FID 
-    join_fid_count_stats = arcpy.Statistics_analysis(
-        in_table= fc_case_1_2, 
-        out_table="in_memory/join_fid_count_table",
-        statistics_fields= [["JOIN_FID", "COUNT"]], 
-        case_field="TARGET_FID"
-        )
+# if au.fieldExist(fc_case_1_2, "COUNT_JOIN_FID") or au.fieldExist(fc_case_1_2, "COUNT_TRAGET_FID"):
+#     arcpy.AddMessage(f"The fields COUNT_JOIN_FID and COUNT_TARGET_FID exists in {os.path.basename(fc_case_1_2)}. Continue... ")
+# else:
+#     # calculate count JOIN_FID 
+#     join_fid_count_stats = arcpy.Statistics_analysis(
+#         in_table= fc_case_1_2, 
+#         out_table="in_memory/join_fid_count_table",
+#         statistics_fields= [["JOIN_FID", "COUNT"]], 
+#         case_field="TARGET_FID"
+#         )
 
-    # add COUNT_JOIN_FID as field to fc
-    arcpy.JoinField_management(
-        in_data=fc_case_1_2, 
-        in_field="TARGET_FID", 
-        join_table=join_fid_count_stats,
-        join_field="TARGET_FID", 
-        fields="COUNT_JOIN_FID"
-        )
+#     # add COUNT_JOIN_FID as field to fc
+#     arcpy.JoinField_management(
+#         in_data=fc_case_1_2, 
+#         in_field="TARGET_FID", 
+#         join_table=join_fid_count_stats,
+#         join_field="TARGET_FID", 
+#         fields="COUNT_JOIN_FID"
+#         )
 
-    # calculate count TARGET_FID 
-    target_fid_count_stats = arcpy.Statistics_analysis(
-        fc_case_1_2, 
-        "in_memory/target_fid_count_table", 
-        [["TARGET_FID", "COUNT"]], 
-        "JOIN_FID"
-        )
-    # add COUNT_TARGET_FID as field to fc 
-    arcpy.JoinField_management(
-        fc_case_1_2, 
-        "JOIN_FID", 
-        target_fid_count_stats, 
-        "JOIN_FID", 
-        "COUNT_TARGET_FID"
-        )
+#     # calculate count TARGET_FID 
+#     target_fid_count_stats = arcpy.Statistics_analysis(
+#         fc_case_1_2, 
+#         "in_memory/target_fid_count_table", 
+#         [["TARGET_FID", "COUNT"]], 
+#         "JOIN_FID"
+#         )
+#     # add COUNT_TARGET_FID as field to fc 
+#     arcpy.JoinField_management(
+#         fc_case_1_2, 
+#         "JOIN_FID", 
+#         target_fid_count_stats, 
+#         "JOIN_FID", 
+#         "COUNT_TARGET_FID"
+#         )
 
-# Select features in class 1
-if arcpy.Exists(fc_case_1):
-    arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_1)} already exists. Continue...")
-else:   
-    query = "COUNT_JOIN_FID_1 > 1 And COUNT_TARGET_FID_1 = 1"  
-    arcpy.management.SelectLayerByAttribute(
-        in_layer_or_view="lyr2",
-        selection_type="NEW_SELECTION",
-        where_clause=query,
-        invert_where_clause=None
-    )
-    arcpy.CopyFeatures_management("lyr2", fc_case_1)
-    logging.info(f'Case 1 selected and exported to {os.path.basename(fc_case_1)}')
+# # Select features in class 1
+# if arcpy.Exists(fc_case_1):
+#     arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_1)} already exists. Continue...")
+# else:   
+#     query = "COUNT_JOIN_FID_1 > 1 And COUNT_TARGET_FID_1 = 1"  
+#     arcpy.management.SelectLayerByAttribute(
+#         in_layer_or_view="lyr2",
+#         selection_type="NEW_SELECTION",
+#         where_clause=query,
+#         invert_where_clause=None
+#     )
+#     arcpy.CopyFeatures_management("lyr2", fc_case_1)
+#     logging.info(f'Case 1 selected and exported to {os.path.basename(fc_case_1)}')
 
-# Select features in class 2
-if arcpy.Exists(fc_case_2):
-    arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_2)} already exists. Continue...")
-else:   
-    # Select features in class 1
-    query = "COUNT_JOIN_FID_1 = 1 And COUNT_TARGET_FID_1 = 1"
-    arcpy.management.SelectLayerByAttribute(
-        in_layer_or_view="lyr2",
-        selection_type="NEW_SELECTION",
-        where_clause=query,
-        invert_where_clause=None
-    )
-    arcpy.CopyFeatures_management("lyr2", fc_case_2)
-    logging.info(f'Case 2 selected and exported to {os.path.basename(fc_case_2)}')   
+# # Select features in class 2
+# if arcpy.Exists(fc_case_2):
+#     arcpy.AddMessage(f"\tFeature {os.path.basename(fc_case_2)} already exists. Continue...")
+# else:   
+#     # Select features in class 1
+#     query = "COUNT_JOIN_FID_1 = 1 And COUNT_TARGET_FID_1 = 1"
+#     arcpy.management.SelectLayerByAttribute(
+#         in_layer_or_view="lyr2",
+#         selection_type="NEW_SELECTION",
+#         where_clause=query,
+#         invert_where_clause=None
+#     )
+#     arcpy.CopyFeatures_management("lyr2", fc_case_2)
+#     logging.info(f'Case 2 selected and exported to {os.path.basename(fc_case_2)}')   
 
 #------------------------------------------------------ #
 # EXPORT to case number count to CSV
