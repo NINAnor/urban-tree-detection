ff373795c6a7c4ad61ff3c27ad6e43acea4aafc0
willeke.acampo <willeke.acampo@nina.no>
2023-05-11 10:05:46 +0200

    update imports

--- src/test/test_config.py
+++ src/test/test_config.py
@@ -1,14 +1,14 @@
 import logging 
 
 # local packages
-from src import setup_logger 
-from src import FKB_BUILDING_PATH, FKB_WATER_PATH, SSB_DISTRICT_PATH, AR5_LANDUSE_PATH
-from src import DATA_PATH, RAW_PATH, INTERIM_PATH, PROCESSED_PATH
-from src import LOG_PATH
+from src import logger
+from src import (FKB_BUILDING_PATH, FKB_WATER_PATH, SSB_DISTRICT_PATH, AR5_LANDUSE_PATH, 
+                 DATA_PATH, RAW_PATH, INTERIM_PATH, PROCESSED_PATH)
+
 
 
 # configure logger
-setup_logger(logfile=True)
+logger.setup_logger(logfile=True)
 logging.info('TEST PROJECT CONFIGURATION')
 logging.info('TEST logger setup')
 logging.debug('Debug message')
@@ -23,10 +23,10 @@ logging.info('TEST project variables')
 print(f" FKB_BUILDING_PATH: \t{FKB_BUILDING_PATH} \n",
       f"FKB_WATER_PATH: \t{FKB_WATER_PATH} \n", 
       f"SSB_DISTRICT_PATH: \t{SSB_DISTRICT_PATH} \n",
-      f"AR5_LANDUSE_PATH: \t{SSB_DISTRICT_PATH} \n",
+      f"AR5_LANDUSE_PATH: \t{AR5_LANDUSE_PATH} \n",
       f"DATA_PATH: \t{DATA_PATH} \n",
       f"RAW_PATH: \t{RAW_PATH} \n",
       f"INTERIM_PATH: \t{INTERIM_PATH} \n",
-      f"PROCESSED_PATH: \t{PROCESSED_PATH} \n",
-      f"LOG_PATH: \t{LOG_PATH} \n")
+      f"PROCESSED_PATH: \t{PROCESSED_PATH} \n"
+      )
       
