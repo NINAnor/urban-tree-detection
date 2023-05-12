3b1b1486040ebc303fdc1781b2c24279c168602b
willeke.acampo <willeke.acampo@nina.no>
2023-05-12 09:21:24 +0200

    add(src/integration): voronoi geometry model

--- src/integration/crown_geometry/case_2_voronoi_tesselation.py
+++ src/integration/crown_geometry/case_2_voronoi_tesselation.py
@@ -13,28 +13,42 @@ import math
 from arcpy import env
 from arcpy.sa import *
 
-## TODO 
+# local modules
+from src import (SPATIAL_REFERENCE, URBAN_TREES_GDB)
+from src import arcpy_utils as au
+
+
+ds_joined_trees = os.path.join(URBAN_TREES_GDB, "joined_trees")
+
+# input data
+fc_case_2_stems = os.path.join(ds_joined_trees, "join_case_2_stems") 
+fc_case_2_crowns = os.path.join(ds_joined_trees, "join_case_2") 
+
+# output data
+fc_case_2_crowns_voronoi = os.path.join(ds_joined_trees, "join_case_2_crowns_voronoi") 
 
   
-## workspace settings
+# env settings
 env.overwriteOutput = True
-env.outputCoordinateSystem = arcpy.SpatialReference("ETRS 1989 UTM Zone 33N")
-env.workspace = r"C:\Users\zofie.cimburova\NINA\15885100 - SIS - 2018 - URBAN Nature values PhD - Dokumenter\4. Data\i-Tree\DATA\Trees\DATA\input_trees.gdb"
+env.outputCoordinateSystem = arcpy.SpatialReference(SPATIAL_REFERENCE)
+env.workspace = ds_joined_trees
+
    
 ## input data
 # problematic tree crowns (with more than one BYM trunk)
-v_crowns = "problematic_crowns"
+v_crowns = fc_case_2_crowns
 l_crowns = arcpy.MakeFeatureLayer_management (v_crowns, "problematic_crowns_layer")
 
 # problematic tree trunks (inside problematic crowns)
-v_trunks = "problematic_points"
+v_trunks = fc_case_2_stems
 l_trunks = arcpy.MakeFeatureLayer_management (v_trunks, "problematic_points_layer")
 
 ## output data
-# split problematic tree crowns
-v_crowns_split = "output_crowns"
-out_path = r"C:\Users\zofie.cimburova\NINA\15885100 - SIS - 2018 - URBAN Nature values PhD - Dokumenter\4. Data\i-Tree\DATA\Trees\DATA\input_trees.gdb"
-arcpy.CreateFeatureclass_management (out_path, v_crowns_split, "POLYGON")
+arcpy.CreateFeatureclass_management(
+    out_path=ds_joined_trees,
+    out_name = "join_case_2_crowns_voronoi", 
+    geometry_type="POLYGON"
+    )
 
 ## Go through crowns and split by Thiessen polygons of trunks 
 env.workspace = r"in_memory"
@@ -62,7 +76,12 @@ for row in cursor:
     arcpy.Delete_management(v_thiessen)
     
     # append to already processed crowns
-    arcpy.Append_management (v_crown_split, out_path+"\\"+v_crowns_split, "NO_TEST")
+    arcpy.Append_management (
+        inputs=v_crown_split,
+        target= fc_case_2_crowns_voronoi, 
+        schema_type= "NO_TEST")
+    
+    # delete temp layer
     arcpy.Delete_management(v_crown_split)
 
 
